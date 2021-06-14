72baac6c51731b4b4e35cd4b20a511b74e77aa48
Alexander Belyavsky <andeithms@yandex.ru>
2021-06-14 21:21:43 +0300

    added permission

--- drf-auth-and-validation/api_with_restrictions/advertisements/views.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
 from advertisements.filters import AdvertisementFilter
+from .permissions import AccessPermission
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -16,6 +17,8 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create"]:
             return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), AccessPermission]
         return []
